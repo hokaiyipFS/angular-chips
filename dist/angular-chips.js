10e839d6c982fedc623b8d07a20168092e41ec16
hokaiyipFS <ho.kai.yip@fullstackers.us>
2016-11-09 11:01:00 -0500

    Update angular-chips.js

--- dist/angular-chips.js
+++ dist/angular-chips.js
@@ -267,40 +267,6 @@
 
 (function() {
     angular.module('angular.chips')
-        .factory('DomUtil', function() {
-            return DomUtil;
-        });
-    /*Dom related functionality*/
-    function DomUtil(element) {
-        /*
-         * addclass will append class to the given element
-         * ng-class will do the same functionality, in our case
-         * we don't have access to scope so we are using this util methods
-         */
-        var utilObj = {};
-
-        utilObj.addClass = function(className) {
-            utilObj.removeClass(element, className);
-            element.attr('class', element.attr('class') + ' ' + className);
-            return utilObj;
-        };
-
-        utilObj.removeClass = function(className) {
-            var classes = element.attr('class').split(' ');
-            var classIndex = classes.indexOf(className);
-            if (classIndex !== -1) {
-                classes.splice(classIndex, 1);
-            }
-            element.attr('class', classes.join(' '));
-            return utilObj;
-        };
-
-        return utilObj;
-    }
-})();
-
-(function() {
-    angular.module('angular.chips')
         .directive('chipTmpl', ChipTmpl);
 
     function ChipTmpl() {
@@ -393,6 +359,40 @@
 })();
 
 (function() {
+    angular.module('angular.chips')
+        .factory('DomUtil', function() {
+            return DomUtil;
+        });
+    /*Dom related functionality*/
+    function DomUtil(element) {
+        /*
+         * addclass will append class to the given element
+         * ng-class will do the same functionality, in our case
+         * we don't have access to scope so we are using this util methods
+         */
+        var utilObj = {};
+
+        utilObj.addClass = function(className) {
+            utilObj.removeClass(element, className);
+            element.attr('class', element.attr('class') + ' ' + className);
+            return utilObj;
+        };
+
+        utilObj.removeClass = function(className) {
+            var classes = element.attr('class').split(' ');
+            var classIndex = classes.indexOf(className);
+            if (classIndex !== -1) {
+                classes.splice(classIndex, 1);
+            }
+            element.attr('class', classes.join(' '));
+            return utilObj;
+        };
+
+        return utilObj;
+    }
+})();
+
+(function() {
     ChipControlLinkFun.$inject = ["scope", "iElement", "iAttrs", "chipsCtrl"];
     angular.module('angular.chips')
         .directive('chipControl', ChipControl);
@@ -411,7 +411,11 @@
     /*@ngInject*/
     function ChipControlLinkFun(scope, iElement, iAttrs, chipsCtrl) {
         iElement.on('keypress', function(event) {
-            if (event.keyCode === 13 && event.target.value !== '') {
+          if (( event.keyCode === 13 ||  // enter
+                event.keyCode === 59 || // semicolon
+                event.keyCode === 32 || // space
+                event.keyCode === 44 ) // comma
+                && event.target.value !== '') {
                 chipsCtrl.addChip(event.target.value);
                 event.target.value = "";
                 event.preventDefault();
